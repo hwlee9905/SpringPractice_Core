585b95593714b3b81a59b0634485bcf77ee30b9f
edwin-commits <9905hyun@naver.com>
2024-01-02 17:20:27 +0900

    study update

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,11 +1,13 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+public class NetworkClient{
     private String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -16,10 +18,23 @@ public class NetworkClient {
         System.out.println("connect : " + url);
     }
     public void call(String message){
-        System.out.println("call : " + url + "message = " + message);
+        System.out.println("call : " + url + " message = " + message);
     }
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
